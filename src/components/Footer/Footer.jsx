e4a286466f6ff5e4012e1324e9018d0856b57c3b
Geovana Matias R <120607451+GeovanaMat@users.noreply.github.com>
2024-09-02 01:27:11 -0400

    ajuste de imagem e reponsividade

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,15 @@
 import "./index.css"
-import github from "/public/github.png"
 const Footer = () => {
     return(
         <footer>
             <div className="footer_container">
                 <p>Desenvolvido por <span>Geovana Matias</span></p>
                 <div className="social_container">
-                <img src={github}/>   
-                <img src="https://img.icons8.com/?size=100&id=8808&format=png&color=000000"/>   
+                <a href="https://github.com/GeovanaMat">
+                <img src="https://img.icons8.com/?size=100&id=12599&format=png&color=000000"/>   
+                </a>
+                <a href="https://www.linkedin.com/in/geovana-matias-comp/"><img src="https://img.icons8.com/?size=100&id=8808&format=png&color=1D1D1D"/> </a>
+                  
                 </div>
                
                 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -15,8 +15,11 @@ const Home = () => {
                 </h1>
             <p>Dedicada a criar interfaces de usuário modernas, intuitivas e responsivas.</p>
             <div className={style.social_container}>
+                <a href="https://github.com/GeovanaMat">
                 <img src="https://img.icons8.com/?size=100&id=12599&format=png&color=000000"/>   
-                <img src="https://img.icons8.com/?size=100&id=8808&format=png&color=1D1D1D"/>   
+                </a>
+                <a href="https://www.linkedin.com/in/geovana-matias-comp/"><img src="https://img.icons8.com/?size=100&id=8808&format=png&color=1D1D1D"/> </a>
+                  
                 </div>
         </div>
 
