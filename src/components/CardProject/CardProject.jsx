ef3584c92a03e4def8b909529f1731c7746b8549
Geovana Matias R. <120607451+GeovanaMat@users.noreply.github.com>
2024-09-01 21:37:58 -0400

    Update CardProject.jsx

--- src/components/CardProject/CardProject.jsx
+++ src/components/CardProject/CardProject.jsx
@@ -4,7 +4,7 @@ const CardProject = () => {
         <>
         <div className={style.card_container}>
                 <div className={style.img_containe}>
-                    <img src="https://github.com/GeovanaMat/portifolio-pessoal/blob/701ecae66429a1c78e68d4621102ad67009b5dc0/src/assets/images/4905662.jpg"/>
+                    <img src="src/assets/images/4905662.jpg"/>
                 </div>
                 <h1>Less is more</h1>
                 <p>Website landing page</p>
